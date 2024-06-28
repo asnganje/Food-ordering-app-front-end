e00836a7314aea8dbe3bd011deea6f289a13c109
asnganje <abd.nganje@gmail.com>
2024-06-28 17:12:45 +0300

    Add relevant pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useSelector } from "react-redux"
+import { Landing } from "./pages";
 
 function App() {
 
   const user = useSelector((store)=>store.user)
   console.log(user);
   return (
-    <div className="text-blue-300">App</div>
+    <div className="text-blue-300">
+      App
+      <Landing />
+    </div>
   )
 }
 export default App
