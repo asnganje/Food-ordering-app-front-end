f83957551ddf82b7abaeaced3af2a32a9963b6b2
asnganje <abd.nganje@gmail.com>
2024-06-30 08:22:49 +0300

    Add links to utils and export

---
+++ src/utils/links.js
@@ -0,0 +1,7 @@
+export const links = [
+    {href:"/", label: 'home'},
+    {href:"about", label: 'about'},
+    {href:"foods", label: 'foods'},
+    {href:"hotels", label: 'hotels'},
+    {href:"cities", label: 'cities'},
+]
