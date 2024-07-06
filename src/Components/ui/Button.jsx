b786f8e6ec184fff92b2399e04d75cd8d7a86bb6
asnganje <abd.nganje@gmail.com>
2024-07-06 20:27:36 +0300

    Modify button styling

--- src/Components/ui/Button.jsx
+++ src/Components/ui/Button.jsx
@@ -1,6 +1,6 @@
 function Button({btnName, handleClick, children}){
 return (
-    <button onClick={handleClick} className="text-xs sm:text-sm">{children?children:btnName}</button>
+    <button onClick={handleClick} className="text-white rounded-md font-serif text-lg bg-orange-500 py-2 px-4 capitalize">{children?children:btnName}</button>
   )
 }
 export default Button;
