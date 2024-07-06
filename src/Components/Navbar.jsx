c6bb48046f1d10a8fc89f3cd06248343c9f9aab0
asnganje <abd.nganje@gmail.com>
2024-07-06 20:27:05 +0300

    Remove mode toggle

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,16 @@
 import HotelButton from "./HotelButton"
 import LinksDropdown from "./LinksDropdown"
 import Logo from "./Logo"
-import ModeToggle from "./ModeToggle"
 import NavLinks from "./NavLinks"
 
 function Navbar() {
   return (
-    <nav className="bg-lytBlueBgColor py-4">
-        <div className="align-element flex justify-between items-center">
+    <nav className="bg-lytBlueBgColor align-element py-4 font-serif text-xl">
+        <div className=" flex justify-between items-center">
             <Logo />
             <LinksDropdown />
             <NavLinks />
             <div className="hidden md:flex justify-center items-center gap-x-4">
-                <ModeToggle />
                 <HotelButton />
             </div>
         </div>
