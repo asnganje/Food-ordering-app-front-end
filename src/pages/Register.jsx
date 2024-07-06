b7a65e1419673b8fe1c59746e6ac8cbc8800c652
asnganje <abd.nganje@gmail.com>
2024-07-06 20:28:27 +0300

    Add logo to register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,6 +4,7 @@ import Button from "../Components/ui/Button"
 import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { createUser } from "../redux/thunks/userThunk"
+import { Logo } from "../Components"
 
 function Register() {
   const dispatch = useDispatch()
@@ -34,20 +35,22 @@ function Register() {
 
 
   return (
-    <section className="h-screen grid place-items-center">
-    <Form onSubmit={handleSubmit}>
-      <FormInput label="first name" value={firstName} changeHandler={changeHandler} name='first name' type='text' placeHolder='First name'/>
-      <FormInput label="Last name" value={lastName} changeHandler={changeHandler} name='last name' type='text' placeHolder='Last name'/>
-      <FormInput label="Email" name='email' value={email} changeHandler={changeHandler} type='email' placeHolder='Email'/>
-      <FormInput label="Password" name='password' value={password} changeHandler={changeHandler} type='password' placeHolder='Password'/>
-      <Button btnName='Register'/>
-      <p className="text-center mt-4">
-        Already a member? 
-        <Button>
-          <Link to='/login'>Sign in</Link>
-        </Button>
-      </p>
-    </Form>
+    <section className="h-screen bg-authBg grid place-items-center">
+      <Form onSubmit={handleSubmit} className="max-w-[450px] rounded-md px-4 py-2 bg-white shadow-xl border-t-gray-600 border-t-4">
+          <Logo />
+          <h4 className="text-center font-serif font-bold uppercase py-4">Register</h4>
+          <FormInput label="first name" value={firstName} changeHandler={changeHandler} name='first name' type='text' placeHolder='First name'/>
+          <FormInput label="Last name" value={lastName} changeHandler={changeHandler} name='last name' type='text' placeHolder='Last name'/>
+          <FormInput label="Email" name='email' value={email} changeHandler={changeHandler} type='email' placeHolder='Email'/>
+          <FormInput label="Password" name='password' value={password} changeHandler={changeHandler} type='password' placeHolder='Password'/>
+          <Button btnName='Register'/>
+          <p className="flex items-center justify-center gap-x-10 text-center font-serif mt-4">
+            Already a member? 
+            <Button>
+              <Link to='/login'>Sign in</Link>
+            </Button>
+          </p>
+      </Form>
     </section>
   )
 }
