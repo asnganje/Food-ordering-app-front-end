ce0e4fc8b5beda62f3da63950b7e5bdcfaebd197
asnganje <abd.nganje@gmail.com>
2024-07-06 16:06:16 +0300

    Add user state management logic

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,19 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createUser, loginUser, logoutUser } from "../thunks/userThunk";
 
+
+const getUserFromLocalStorage = () => {
+  const user = localStorage.getItem('user')
+  if (!user) return null
+  return JSON.parse(user)
+}
 const initialState = {
-  name: 'Abdul',
-  isActive: false
+  isLoading: true,
+  user: getUserFromLocalStorage()
 }
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {
-    logoutUser: (state)=> {
-      state.name=null
-    }
+  extraReducers(builder) {
+    builder.addCase(createUser.pending, (state)=> {
+      state.isLoading = true
+    });
+    builder.addCase(createUser.fulfilled, (state)=> {
+      state.isLoading = false
+    });
+    builder.addCase(createUser.rejected, (state)=> {
+      state.isLoading = false
+    });
+    builder.addCase(loginUser.pending, (state)=> {
+      state.isLoading = true
+    });
+    builder.addCase(loginUser.fulfilled, (state, action)=> {
+      state.isLoading = false
+      const user = action.payload
+      state.user = user
+      localStorage.setItem('user', JSON.stringify(user))
+    });
+    builder.addCase(loginUser.rejected, (state)=> {
+      state.isLoading = false
+    });
+    builder.addCase(logoutUser.pending, (state) => {
+      state.isLoading = true;
+  });
+  builder.addCase(logoutUser.fulfilled, (state) => {
+      state.isLoading = false;
+      state.user = null;
+      localStorage.removeItem('user');
+  });
+  builder.addCase(logoutUser.rejected, (state) => {
+      state.isLoading = false;
+  });
   }
 })
 
 export const userReducer = userSlice.reducer
-export const {logoutUser} = userSlice.actions
