f04c5765176e01f29694174947314becceeaa944
asnganje <abd.nganje@gmail.com>
2024-07-06 20:25:58 +0300

    Add image to hotel component

--- src/Components/HotelButton.jsx
+++ src/Components/HotelButton.jsx
@@ -1,6 +1,10 @@
+import { Link } from 'react-router-dom'
+import food1 from '../assets/food1.png'
 function HotelButton() {
   return (
-    <div>HotelButton</div>
+    <Link to='/'>
+      <img src={food1} alt="food1" className="w-12 h-12 rounded-full bg-transparent text-orange-500"/>
+    </Link>
   )
 }
 export default HotelButton
