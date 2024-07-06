7b6fd308b0f98361874b60d679942f0375a78c7b
asnganje <abd.nganje@gmail.com>
2024-07-06 20:25:14 +0300

    Modify tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ export default {
   theme: {
     extend: {
       colors: {
+        btnBg: '#2cb1bc',
+        authBg: '#f8fafc',
         lytBlueBgColor: 'rgba(235, 244, 255, 1.0)',
         logoBgColor: 'rgba(15, 23, 42, 1.0)'
       }
