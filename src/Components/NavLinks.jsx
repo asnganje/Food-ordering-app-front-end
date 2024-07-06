13adc6b9277e44be141defab8ef1545943284de0
asnganje <abd.nganje@gmail.com>
2024-07-06 16:04:25 +0300

    Add logic for dynamic handling of navlinks

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -1,10 +1,17 @@
 import { NavLink } from "react-router-dom"
 import { links } from "../utils/links"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { hideMenu } from "../redux/slices/dropDownSlice"
 
 function NavLinks() {
   const dispatch = useDispatch()
+  const store = useSelector((store)=>store.user)
+  const {user} = store
+  
+  let updatedLinks = links
+  if (!user) {
+    updatedLinks = updatedLinks.filter((item)=>item.label !== 'About')
+  }
 
   const hideDropdownMenu = () => {
     dispatch(hideMenu())
@@ -12,9 +19,9 @@ function NavLinks() {
   return (
     <div className="hidden md:flex justify-between items-center gap-[2rem]">
       {
-        links.map((link)=> {
+        updatedLinks.map((link)=> {
             return( <NavLink to={link.href} key={link.label} 
-              
+
                           className={({isActive})=> {
                             return `hover:underline font-serif font-light ${isActive? 'text-blue-700' : 'text-gray-500'}`
                          }}
