a4808794829ee5fd575810352d353137b707a924
asnganje <abd.nganje@gmail.com>
2024-07-06 20:27:56 +0300

    Modify form input content

--- src/Components/ui/FormInput.jsx
+++ src/Components/ui/FormInput.jsx
@@ -1,8 +1,8 @@
 function FormInput({label, name, type, value, changeHandler, placeHolder}) {
   return (
-    <div className='mb-2'>
-        <label htmlFor={name}  className="capitalize"> {label || name}</label>
-        <input id={name} name={name} onChange={(e)=>changeHandler(e)} value={value} type={type} placeholder={placeHolder}></input>
+    <div className='grid mb-5'>
+        <label htmlFor={name}  className="w-[50vw] font-serif capitalize"> {label || name}</label>
+        <input id={name} name={name} className="border-[1px] h-[2.5rem] font-sans font-light rounded-sm shadow-sm border-lytBlueBgColor  w-[40vw] lg:w-[25vw]" onChange={(e)=>changeHandler(e)} value={value} type={type} placeholder={placeHolder}></input>
       </div>
   )
 }
