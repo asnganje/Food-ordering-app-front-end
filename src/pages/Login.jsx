e4991198997c1b6404d86e71bef80079a46b4eb9
asnganje <abd.nganje@gmail.com>
2024-07-06 20:28:14 +0300

    Add logo to login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,7 @@ import FormInput from "../Components/ui/FormInput"
 import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { loginUser } from "../redux/thunks/userThunk"
+import { Logo } from "../Components"
 
 function Login() {
   const navigate = useNavigate()
@@ -25,13 +26,15 @@ function Login() {
     navigate('/about')
   }
   return (
-    <section className="h-screen grid place-items-center">
-    <Form onSubmit={loginHandler}>
+    <section className="h-screen bg-authBg grid place-items-center">
+    <Form onSubmit={loginHandler} className="max-w-[450px] rounded-md px-4 py-6 bg-white shadow-xl border-t-gray-600 border-t-4">
+    <Logo />
+    <h4 className="text-center font-serif font-bold uppercase py-4">Sign In</h4>
       <FormInput label="Email" name='email' value={email} changeHandler={changeHandler} type='email' placeHolder='Email'/>
       <FormInput label="Password" name='password' value={password} changeHandler={changeHandler} type='password' placeHolder='Password'/>
       <Button btnName='Sign in'/>
 
-      <p className="text-center mt-4">
+      <p className="flex items-center justify-center gap-x-10 text-center font-serif mt-4">
         Not a member yet? 
         <Button>
           <Link to='/register'>Register</Link>
