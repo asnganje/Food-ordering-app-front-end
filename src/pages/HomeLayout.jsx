d75a6d37cbb9ecaf1e58c130d7dd54a43fd8713c
asnganje <abd.nganje@gmail.com>
2024-07-07 11:55:04 +0300

    Modify layout

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -5,17 +5,19 @@ import { useSelector } from "react-redux"
 function HomeLayout() {
   const {isVisible} = useSelector((store)=>store.menu)
   return (
-    <main className="bg-[url(src/assets/bg.png)] bg-cover min-h-[60vh]">
-      <Header />
-      <Navbar />
-      <>
-      { isVisible?
-        <Dropdown/>:
-      <div className="align-element ">
-        <Outlet />
+    <main className=" min-h-screen">
+      <div className="bg-[url(src/assets/bg.png)] bg-cover max-h-[65vh] lg:max-h-[60vh]">
+          <Header />
+          <Navbar />
+          <>
+          { isVisible?
+            <Dropdown/>:
+          <div className="align-element ">
+            <Outlet />
+          </div>
+          }
+          </>
       </div>
-      }
-      </>
     </main>
   )
 }
