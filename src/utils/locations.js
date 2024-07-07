18651645159773a75f0520c641de5036c036fc5a
asnganje <abd.nganje@gmail.com>
2024-07-07 11:54:01 +0300

    Add locations list

---
+++ src/utils/locations.js
@@ -0,0 +1,30 @@
+export const locations = [
+  {
+    id:1,
+    label: 'Nairobi'
+  },
+  {
+    id:2,
+    label: 'Mombasa'
+  },
+  {
+    id:3,
+    label: 'Addis Ababa'
+  },
+  {
+    id:4,
+    label: 'Mumbai'
+  },
+  {
+    id:5,
+    label: 'Dar es Salam'
+  },
+  {
+    id:6,
+    label: 'Delhi'
+  },
+  {
+    id:7,
+    label: 'Bangalore'
+  },
+]
