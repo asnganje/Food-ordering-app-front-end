ace4fc2cd47caa78daccf3421b38c863a2420fe1
asnganje <abd.nganje@gmail.com>
2024-07-07 11:54:19 +0300

    Add quick products array

---
+++ src/utils/qProducts.js
@@ -0,0 +1,45 @@
+import drinks from "../assets/drinks.png"
+import samosa from "../assets/samosa.png"
+import shaheepaneer from "../assets/shaheepaneer.png"
+import nightlife from "../assets/nightlife.png"
+import idli from "../assets/idli.png"
+import pizza from "../assets/pizza.png"
+
+export const quickProducts = [
+  {
+    id:1,
+    meal: drinks,
+    label: 'Night LifeBuoy',
+    description: 'end your day with exclusive best Nightlife options'
+  },
+  {
+    id:2,
+    meal: samosa,
+    label: 'Indian',
+    description: 'Start your evening with exclusive Indian Snacks Options'
+  },
+  {
+    id:3,
+    meal: shaheepaneer,
+    label: 'North Indian',
+    description: 'Start your afternoon with exclusive North Indian lunch options'
+  },
+  {
+    id:4,
+    meal: nightlife,
+    label: 'Chinese',
+    description: 'Start your evening with exclusive chinese options.'
+  },
+  {
+    id:5,
+    meal: idli,
+    label: 'South Indian',
+    description: 'Start your afternoon with exclusive south Indian lunch options'
+  },
+  {
+    id:6,
+    meal: pizza,
+    label: 'Fast food',
+    description: 'Start your day with exclusive breakfast options'
+  },
+]
