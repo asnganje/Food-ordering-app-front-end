35ee6202b224bf84ac15a4ba275f28e8d4940b57
asnganje <abd.nganje@gmail.com>
2024-07-07 08:21:20 +0300

    Export components from index

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,7 @@
-export {default as Header} from "./Header"
-export {default as Navbar} from "./Navbar"
-export {default as NavLinks} from "./NavLinks"
-export {default as Logo} from "./Logo"
-export {default as LinksDropdown} from "./LinksDropdown"
-export {default as HotelButton} from "./HotelButton"
-export {default as Dropdown} from "./Dropdown"
+export { default as Header } from "./Header";
+export { default as Navbar } from "./Navbar";
+export { default as NavLinks } from "./NavLinks";
+export { default as Logo } from "./Logo";
+export { default as LinksDropdown } from "./LinksDropdown";
+export { default as HotelButton } from "./CartButton";
+export { default as Dropdown } from "./Dropdown";
