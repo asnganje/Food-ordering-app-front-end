36ff88069cbdabcacd4f6dad722f8a040992b11e
asnganje <abd.nganje@gmail.com>
2024-07-07 08:21:30 +0300

    Modify navbar content

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,20 @@
-import HotelButton from "./HotelButton"
-import LinksDropdown from "./LinksDropdown"
-import Logo from "./Logo"
-import NavLinks from "./NavLinks"
+import CartButton from "./CartButton";
+import LinksDropdown from "./LinksDropdown";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 function Navbar() {
   return (
     <nav className="bg-lytBlueBgColor align-element py-4 font-serif text-xl">
-        <div className=" flex justify-between items-center">
-            <Logo />
-            <LinksDropdown />
-            <NavLinks />
-            <div className="hidden md:flex justify-center items-center gap-x-4">
-                <HotelButton />
-            </div>
+      <div className=" flex justify-between items-center">
+        <Logo />
+        <LinksDropdown />
+        <NavLinks />
+        <div className="hidden md:flex justify-center items-center gap-x-4">
+          <CartButton />
         </div>
+      </div>
     </nav>
-  )
+  );
 }
-export default Navbar
+export default Navbar;
