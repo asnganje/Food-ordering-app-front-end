dc9bd98ef081034230f830fe8195b8b842036830
asnganje <abd.nganje@gmail.com>
2024-07-08 07:31:32 +0300

    Add about page content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,19 @@
 function About() {
   return (
-    <div>About</div>
+    <section className="bg-none">
+    <h1 className="flex flex-wrap text-blue-950 gap-2 sm:gap-x-6 my-[5vh] items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+    Crave tasty <span className="bg-orange-500 py-3 px-4 rounded-lg tracking-widest text-white">
+        bites!
+      </span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto">
+      Welcome to our oasis, where every stay is a culinary adventure. 
+      Our hotel is perfect for those who crave tasty bites. 
+      From our elegantly designed lobby to our chef's signature dishes, 
+      we blend comfort with exceptional dining. 
+      Join us and let your culinary journey begin.
+      </p>
+  </section>
   )
 }
 
