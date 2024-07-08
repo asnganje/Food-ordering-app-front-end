c5040202c512f07d01b391f1e319b423fb5a3ef3
asnganje <abd.nganje@gmail.com>
2024-07-08 07:31:20 +0300

    Modify product styling

--- src/Components/QuickProducts.jsx
+++ src/Components/QuickProducts.jsx
@@ -6,7 +6,7 @@ function QuickProducts() {
       {quickProducts.map((product)=> {
         const { id, meal, label, description} = product
         return (
-                <div key={id} className="grid grid-cols-2 gap-4 bg-white shadow-xl cursor-pointer">
+                <div key={id} className="grid grid-cols-2 gap-4 bg-white shadow-xl z-0 hover:mr-8 transition-all duration-500 hover:z-50 hover:shadow-2xl cursor-pointer">
                   <img src={meal} alt="label" className="h-[10rem] w-[10rem] rounded-md"/>
                   <div>
                     <h3 className="font-sans text-xl font-bold">{label}</h3>
